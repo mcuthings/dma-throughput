c4bc392d6f177cede165a3c22254d07f77ceb102
Hiroshi Hiraga <tw-mcuthings@mcu-things.com>
2018-10-19 20:21:22 +0900

    removed and left C main source on accordance with software license.

--- frdmk64f_driver_examples_edma_edma_memory_to_memory/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := /Users/Hiro-MBP/Freescale/workspaces/k64/frdmk64f_driver_examples_edma_edma_memory_to_memory/randomData.o
-
-LIBS :=
-
